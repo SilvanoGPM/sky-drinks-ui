cf6a1bbf4e61c6c9a9e09ca30a9b0d7208d5c2f2
Silvano Marques <silvanosilvino@hotmail.com>
2022-02-16 16:37:44 -0300

    feat: navigation to latest drinks in login page

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,28 +1,33 @@
 const CAHCE_NAME = '@skydrinks-cache';
 
-const URLS_TO_CACHE = ['/', '/index.html'];
+const URLS_TO_CACHE = [];
 
 /* eslint-disable */
 
 self.addEventListener('install', (event) => {
-  event
-  .waitUntil(caches.open(CAHCE_NAME)
-  .then((cache) => {
-    console.log('Opened cache');
-    return cache.addAll(URLS_TO_CACHE);
-  }))
+  event.waitUntil(
+    caches
+      .open(CAHCE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+      .catch(() => {
+        console.log('error');
+      })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  event
-  .respondWith(caches.match(event.request)
-  .then((response) => {
-    if (response) {
-      return response;
-    }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
 
-    return fetch(event.request);
-  }))
+      return fetch(event.request);
+    })
+  );
 });
 
 /* eslint-enable */
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from 'react';
-import { Input, Form, Button, Checkbox, Spin, Typography } from 'antd';
+import { Input, Form, Button, Checkbox, Spin, Typography, Tooltip } from 'antd';
 import { Link, Navigate, useLocation } from 'react-router-dom';
 import { useSpring, animated, useChain, useSpringRef } from 'react-spring';
 
@@ -17,6 +17,7 @@ import { showNotification } from 'src/utils/showNotification';
 import { useFlashNotification } from 'src/hooks/useFlashNotification';
 import { handleError } from 'src/utils/handleError';
 import { ShakeIcon } from 'src/components/other/ShakeIcon';
+import { DrinkIcon } from 'src/components/custom/CustomIcons';
 
 import loginImage from 'src/assets/login-image.jpg';
 import styles from './styles.module.scss';
@@ -98,110 +99,128 @@ export function Login(): JSX.Element {
   }
 
   return (
-    <main className={styles.container}>
-      <animated.section
-        className={styles.formContainer}
-        style={{ ...slideInProps, ...showProps }}
-      >
-        <div className={styles.form}>
-          <div className={styles.header}>
-            <ShakeIcon>
-              <UserOutlined style={{ fontSize: '3rem' }} />
-            </ShakeIcon>
+    <>
+      <main className={styles.mainContainer}>
+        <animated.section
+          className={styles.formContainer}
+          style={{ ...slideInProps, ...showProps }}
+        >
+          <div className={styles.form}>
+            <div className={styles.header}>
+              <ShakeIcon>
+                <UserOutlined style={{ fontSize: '3rem' }} />
+              </ShakeIcon>
 
-            <h1 className={styles.title}>Login</h1>
+              <h1 className={styles.title}>Login</h1>
 
-            <svg
-              className={styles.curve}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1440 320"
+              <svg
+                className={styles.curve}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1440 320"
+              >
+                <path
+                  fill="#ffffff"
+                  fillOpacity="1"
+                  d="M0,96L80,80C160,64,320,32,480,58.7C640,85,800,171,960,181.3C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+                />
+              </svg>
+            </div>
+
+            <Form
+              style={{ padding: '1rem' }}
+              name="login"
+              layout="vertical"
+              onFinish={handleFormLogin}
+              initialValues={{ remember: true }}
             >
-              <path
-                fill="#ffffff"
-                fillOpacity="1"
-                d="M0,96L80,80C160,64,320,32,480,58.7C640,85,800,171,960,181.3C1120,192,1280,128,1360,96L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-              />
-            </svg>
+              <animated.div style={inputProps}>
+                <Form.Item
+                  hasFeedback
+                  label="Email"
+                  name="email"
+                  validateTrigger="onBlur"
+                  rules={[
+                    {
+                      required: true,
+                      type: 'email',
+                      message: 'Insiria um E-mail válido!',
+                    },
+                  ]}
+                >
+                  <Input
+                    prefix={<MailOutlined />}
+                    placeholder="joao@mail.com"
+                  />
+                </Form.Item>
+              </animated.div>
+
+              <animated.div style={inputProps}>
+                <Form.Item
+                  label="Senha"
+                  name="password"
+                  hasFeedback
+                  rules={[{ required: true, message: 'Insira sua senha!' }]}
+                >
+                  <Input.Password prefix={<LockOutlined />} />
+                </Form.Item>
+              </animated.div>
+
+              <animated.div style={inputProps}>
+                <div className={styles.forgotPassword}>
+                  <Link to={routes.FORGOT_PASSWORD}>
+                    <Typography.Link>Esqueceu a senha?</Typography.Link>
+                  </Link>
+                </div>
+              </animated.div>
+
+              <animated.div style={inputProps}>
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>Lembrar de mim</Checkbox>
+                </Form.Item>
+              </animated.div>
+
+              <animated.div style={inputProps}>
+                <Form.Item
+                  wrapperCol={{
+                    span: 24,
+                    offset: 0,
+                  }}
+                >
+                  <Button
+                    style={{ width: '100%' }}
+                    icon={<LoginOutlined />}
+                    type="primary"
+                    htmlType="submit"
+                    loading={authLoading}
+                    shape="round"
+                    size="large"
+                  >
+                    Entrar
+                  </Button>
+                </Form.Item>
+              </animated.div>
+            </Form>
           </div>
 
-          <Form
-            style={{ padding: '1rem' }}
-            name="login"
-            layout="vertical"
-            onFinish={handleFormLogin}
-            initialValues={{ remember: true }}
-          >
-            <animated.div style={inputProps}>
-              <Form.Item
-                hasFeedback
-                label="Email"
-                name="email"
-                validateTrigger="onBlur"
-                rules={[
-                  {
-                    required: true,
-                    type: 'email',
-                    message: 'Insiria um E-mail válido!',
-                  },
-                ]}
-              >
-                <Input prefix={<MailOutlined />} placeholder="joao@mail.com" />
-              </Form.Item>
-            </animated.div>
+          <figure className={styles.figure}>
+            {loadingImage && <Spin />}
+            <img alt="" src={loginImage} onLoad={endImageLoad} />
+          </figure>
+        </animated.section>
+      </main>
 
-            <animated.div style={inputProps}>
-              <Form.Item
-                label="Senha"
-                name="password"
-                hasFeedback
-                rules={[{ required: true, message: 'Insira sua senha!' }]}
-              >
-                <Input.Password prefix={<LockOutlined />} />
-              </Form.Item>
-            </animated.div>
-
-            <animated.div style={inputProps}>
-              <div className={styles.forgotPassword}>
-                <Link to={routes.FORGOT_PASSWORD}>
-                  <Typography.Link>Esqueceu a senha?</Typography.Link>
-                </Link>
-              </div>
-            </animated.div>
-
-            <animated.div style={inputProps}>
-              <Form.Item name="remember" valuePropName="checked">
-                <Checkbox>Lembrar de mim</Checkbox>
-              </Form.Item>
-            </animated.div>
-
-            <animated.div style={inputProps}>
-              <Form.Item
-                wrapperCol={{
-                  span: 24,
-                  offset: 0,
-                }}
-              >
-                <Button
-                  style={{ width: '100%' }}
-                  icon={<LoginOutlined />}
-                  type="primary"
-                  htmlType="submit"
-                  loading={authLoading}
-                  shape="round"
-                  size="large"
-                >
-                  Entrar
-                </Button>
-              </Form.Item>
-            </animated.div>
-          </Form>
-        </div>
-
-        <figure className={styles.figure}>
-          {loadingImage && <Spin />}
-          <img alt="" src={loginImage} onLoad={endImageLoad} />
-        </figure>
-      </animated.section>
-    </main>
+      <div className={styles.bottomButton}>
+        <Tooltip title="Ver últimas bebidas" placement="left">
+          <Link to={routes.HOME}>
+            <Button
+              style={{ minWidth: 50, minHeight: 50 }}
+              shape="circle"
+              type="primary"
+              icon={<DrinkIcon style={{ fontSize: 25 }} />}
+            />
+          </Link>
+        </Tooltip>
+      </div>
+    </>
   );
 }
